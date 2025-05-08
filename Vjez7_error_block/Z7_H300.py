463c0a4ad4ef0760c8607113caa87acba954a41e
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2025-05-08 09:06:00 +0200

    gotovo

--- Vjez7_error_block/Z7_H300.py
+++ Vjez7_error_block/Z7_H300.py
@@ -1,5 +1,7 @@
 import numpy as np
 import warnings
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tick
 
 '''
 U ovom kodu racuna se standardna devijacija srednje udaljenosti iz "rNw_H300.dat".
@@ -7,6 +9,7 @@ U ovom kodu racuna se standardna devijacija srednje udaljenosti iz "rNw_H300.dat
 
 Nw = 10000  #broj setaca
 Nkmax = 200
+
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     frNw = np.loadtxt('rNw_H300.dat', comments='#', max_rows=Nw*200, usecols=(1,2,3))
@@ -30,3 +33,27 @@ for Nk in range(1, Nkmax+1):
     fstdev.write(f"{Nk:<3d} {Smb:>13.9f} {np.sqrt(var):>12.9f}\n")
         
 fstdev.close()
+
+fo = np.loadtxt('st_dev_H300.dat')
+bl, mr, dev = [], [], []
+
+for i in range(Nkmax):
+    bl.append(fo[i, 0])
+    mr.append(fo[i, 1])
+    dev.append(fo[i, 2])
+    
+fig = plt.figure(figsize=(12,5), dpi=110)
+ax = fig.add_axes([0.10, 0.15, 0.85, 0.70])
+plt.rcParams.update({'font.size': 10}) #type: ignore
+ax.errorbar(bl, mr, yerr=dev, fmt='o-', capsize=2, label='data', ecolor='green', elinewidth=0.3,
+            mfc='lime', mec='green', ms=5, mew=1)
+ax.set_xlim(0, 201)
+ax.set_ylim(13.51, 13.52)
+ax.set_xlabel('b / $10^{4}$')
+ax.set_ylabel('<r> / a$_{0}$')
+ax.xaxis.set_major_locator(tick.MultipleLocator(20))
+ax.yaxis.set_major_locator(tick.MultipleLocator(0.001))
+ax.grid(lw=0.2, linestyle=':')
+ax.set_title('Vrijednosti srednje radijalne udaljenosti <r> elektrona u\n|3,0,0> stanju $^{1}$H atoma dobivene dodatnim blokiranjem')
+ax.legend(loc='upper right')
+plt.show()
