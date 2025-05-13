cee5b811b14b0724daed6e1152b4328dcdabe300
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2025-05-13 12:45:20 +0200

    eeeh

--- Vjez8_demon/Z8.py
+++ Vjez8_demon/Z8.py
@@ -3,10 +3,10 @@ import numpy as np
 ES = 20.0 #energija sustava
 N = 80 #broj cestica
 ED = 0.0 #energija demona
-dvMax = 0.4 #maksimalna promjena brzine v
+dvMax = 0.9 #maksimalna promjena brzine v
 burn = 1000 #odbaceni koraci
 Nk = 10000 #ukupni broj koraka
-Nw = 40 #broj setaca
+Nw = 50 #broj setaca
 
 v = np.zeros((N, Nw))
 v[0, :] = np.sqrt(2*ES/N) #pocetne brzine
@@ -16,17 +16,16 @@ def E(vel):
     return 0.5*(vel**2)
 
 accept = 0
-output1 = open('ES.dat', 'w') #korak, energija sustava, ukupna energija sustava
-output2 = open('ED.dat', 'w') #korak, energija demona, ukupna energija demona
-output3 = open('table.txt', 'w') #tablica za ES=20 i N=80
+output1 = open('ES.dat', 'w') #korak, energija sustava po koraku, ukupna energija sustava
+output2 = open('ED.dat', 'w') #korak, energija demona po koraku, ukupna energija demona
 
 dES, dED = 0.0, 0.0
-mES, mED = 0.0, 0.0
 
 for k in range(1, Nk+1):
     ESk, EDk = 0.0, 0.0 #energije po koraku
+    mES, mED = 0.0, 0.0
     for j in range(Nw):
-        ip = int(np.random.rand()*N)
+        ip = int(np.random.rand()*N) #indeks cestice
         if ip == N:
             ip = N-1
         dv = (2*np.random.rand()-1.0)*dvMax
@@ -44,23 +43,26 @@ for k in range(1, Nk+1):
     mED += EDk/Nw
     if k >= burn and k%10 == 0:
         ko = k-burn+1
-        output1.write(f"{k:>6d} {mES/ko+ES:>15.11f} {dES+ES:>15.11f}\n")
-        output2.write(f"{k:>6d} {mED/ko+ED:>15.11f} {dED+ED:>15.11f}\n")
+        output1.write(f"{k:>6d} {mES+ES:>15.11f} {dES+ES:>15.11f}\n")
+        output2.write(f"{k:>6d} {mED+ED:>15.11f} {dED+ED:>15.11f}\n")
 
 output1.close()
 output2.close()
+print("\nacceptance rate = {} %\n".format(accept/(Nk*Nw)*100))
 
-fES = ES+dES #konacna energija po cestici
-fED = ED+dED
+output3 = open('table.txt', 'w') #tablica za E=20 i N=80
+input1 = np.loadtxt('ES.dat', usecols=2)
+input2 = np.loadtxt('ED.dat', usecols=2)
 
-accept /= Nk*Nw
-print(accept*100, fES, fED, fES+fED)
+fES = np.mean(input1) #srednja vrijednost ukupne energije sustava
+fED = np.mean(input2) #srednja vrijednost ukupne energije demona
+print(fES, fED)
 
-output3.write(f"{'N':14s} {N:>8d}\n")
-output3.write(f"{'E':14s} {ES+ED:>8.0f}\n")
-output3.write(f"{'<ED>':14s} {fED:>8.1f}\n")
-output3.write(f"{'<ES>':14s} {fES:>8.1f}\n")
-output3.write(f"{'<ES>/N':14s} {fES/N:>8.5f}\n")
-output3.write(f"{'<ES>/(N*<ED>)':14s} {fES/(N*fED):>8.3f}\n")
-output3.write(f"{'0.5N*<ED>':14s} {0.5*N*fED:>8.0f}\n")
+output3.write(f"{'N':14s} {N:<8d}\n")
+output3.write(f"{'E':14s} {ES+ED:<8.0f}\n")
+output3.write(f"{'<ED>':14s} {fED:<8.1f}\n")
+output3.write(f"{'<ES>':14s} {fES:<8.1f}\n")
+output3.write(f"{'<ES>/N':14s} {fES/N:<8.5f}\n")
+output3.write(f"{'<ES>/(N*<ED>)':14s} {fES/(N*fED):<8.3f}\n")
+output3.write(f"{'0.5N*<ED>':14s} {0.5*N*fED:<8.0f}\n")
 output3.close()
