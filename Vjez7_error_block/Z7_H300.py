dd55369d7b8849540735499e886ab0f26230116f
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2025-05-20 14:26:04 +0200

    sve dobro

--- Vjez7_error_block/Z7_H300.py
+++ Vjez7_error_block/Z7_H300.py
@@ -12,7 +12,7 @@ Nkmax = 200
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
-    fr = np.loadtxt('r_H300.dat', comments='#', usecols=2)
+    fr = np.loadtxt('r_H300.dat', comments='#', usecols=1)
 fstdev = open('st_dev_H300.dat', 'w') #file za graf
 
 N = len(fr)
@@ -39,38 +39,38 @@ bl, mr, dev = [], [], []
 for i in range(len(fo)):
     bl.append(fo[i, 0])
     mr.append(fo[i, 1])
-    dev.append(1000*fo[i, 2])
+    dev.append(fo[i, 2])
     
-'''fig = plt.figure(figsize=(12,5), dpi=110)
+fig = plt.figure(figsize=(12,5), dpi=110)
 ax = fig.add_axes([0.10, 0.15, 0.85, 0.70])
 plt.rcParams.update({'font.size': 10}) #type: ignore
 ax.errorbar(bl, mr, yerr=dev, fmt='o-', capsize=2, label='data', ecolor='green', elinewidth=0.3,
             mfc='lime', mec='green', ms=5, mew=1)
 ax.set_xlim(0, 67)
-ax.set_ylim(13.1, 13.9)
+ax.set_ylim(13.499, 13.503)
 ax.set_xlabel('b')
 ax.set_ylabel('<r> / a$_{0}$')
 ax.xaxis.set_major_locator(tick.MultipleLocator(6))
-ax.yaxis.set_major_locator(tick.MultipleLocator(0.1))
+ax.yaxis.set_major_locator(tick.MultipleLocator(0.0005))
 ax.grid(lw=0.2, linestyle=':')
 ax.set_title('Vrijednosti srednje radijalne udaljenosti <r> elektrona u\n|3,0,0> stanju $^{1}$H atoma dobivene dodatnim blokiranjem')
 ax.legend(loc='upper right')
-plt.show()'''
+plt.show()
 
-mdev = f"{np.mean(dev)/1000:<5.5f}"
+'''mdev = f"{np.mean(dev)/1000:<5.5f}"
 
 fig = plt.figure(figsize=(8,5), dpi=110)
 ax = fig.add_axes([0.10, 0.15, 0.85, 0.70])
 plt.rcParams.update({'font.size': 10}) #type: ignore
 ax.plot(bl, dev, color='green', lw=1.7)
 ax.set_xlim(0, 67)
-ax.set_ylim(0.04, 0.36)
+ax.set_ylim(0.4, 1.4)
 ax.set_xlabel('b')
 ax.set_ylabel('$10^{3} \u03C3_{r}$ / $\u212B$')
 ax.xaxis.set_major_locator(tick.MultipleLocator(6))
-ax.yaxis.set_major_locator(tick.MultipleLocator(0.04))
+ax.yaxis.set_major_locator(tick.MultipleLocator(0.2))
 ax.grid(lw=0.2, linestyle=':')
 ax.set_title('Standardna devijacija $\u03C3_{r}$ radijalne udaljenosti <r> elektrona u\n|3,0,0> stanju $^{1}$H atoma')
 #ax.legend(loc='upper right')
-ax.text(4, 0.32, s='$\u03C3_{}$={} $\u212B$'.format('r', mdev))
-plt.show()
+ax.text(4, 1.3, s='$\u03C3_{}$={} $\u212B$'.format('r', mdev))
+plt.show()'''
